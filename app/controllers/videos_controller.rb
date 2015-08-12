0632108642ad6715a36ee0a5c531d4c6886f709d
Tyler Elric <tyler9725@gmail.com>
2015-08-12 17:51:38 -0500

    Video records management available.

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -3,9 +3,16 @@ class VideosController < ApplicationController
   end
 
   def create
-    render plain: params[:videos][:name]
+    @vidya = Video.new(params.require(:video_data).permit(:name,:url))
+    @vidya.save
+    redirect_to @vidya
+  end
+
+  def show
+    @vidya = Video.find(params[:id])
   end
 
   def index
+    @vidyas = Video.all
   end
 end
