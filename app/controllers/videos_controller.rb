a9bdec0156bb9836eb2045edd15683be8b48f227
Tyler Elric <tyler9725@gmail.com>
2015-08-12 23:29:05 -0500

    Massive update.

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,4 +1,39 @@
 class VideosController < ApplicationController
+
+  def valid_url(url)
+    @default_value = nil
+    @match = /^(?:https?\:\/\/)?(?:www\.)?youtube\.com\/(?:[^?]+)\?(.+)$/.match(url)
+    if @match then
+      @query = @match.captures[0]
+      @id_start_index = @query.index 'v='
+      if @id_start_index.nil? then
+        return @default_value
+      end
+      @id_start_index += 2
+      @id_end_index = @query.index '&', @id_start_index
+      if @id_end_index.nil? then
+        @id_end_index = @query.length
+      end
+      return @query[@id_start_index,@id_end_index-@id_start_index]
+    else
+      @match = /^(?:https?\:\/\/)?(?:www\.)?youtu\.be\/([^?]+)(?:.+)$/.match(url)
+      if @match then
+        return @match.captures[0]
+      else
+        return @default_value
+      end
+    end
+  end
+
+  def index
+    @title = "All Videos"
+    @vidyas = Video.all
+  end
+
+  def show
+    @vidya = Video.find(params[:id])
+  end
+
   def new
     if params.has_key?(:error)
       @errorzy = params[:error]
@@ -6,26 +41,21 @@ class VideosController < ApplicationController
     end
   end
 
-  def valid_url(url)
-    return nil
-  end
-
   def create
     @URL = valid_url params[:video_data][:url]
     if not @URL then
       redirect_to :controller=>"videos", :action => "new", :error=>"Invalid URL", :bad_url => params[:video_data][:url]
     else
-      @vidya = Video.new :name=>params[:name], url:@URL
+      @vidya = Video.new :name=>params[:video_data][:name], url:@URL
       @vidya.save
       redirect_to @vidya
     end
   end
 
-  def show
+  def destroy
     @vidya = Video.find(params[:id])
+    @vidya.destroy
+    redirect_to videos_path
   end
 
-  def index
-    @vidyas = Video.all
-  end
 end
